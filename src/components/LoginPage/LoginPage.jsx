e5ecce17deae6dd127154136afec999ed01a7933
experimentforludo-tech <experimentforludo@gmail.com>
2026-08-15 15:08:09 +0530

    Update LoginPage.jsx

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,8 +1,14 @@
+import React, { useEffect } from 'react';
 import AddServer from './AddServer/AddServer';
 import JoinServer from './JoinServer/JoinServer';
 import styles from './LoginPage.module.css';
 
-const LoginPage = () => {
+const LoginPage = ({ setRedirect }) => {
+    // 🔥 IMPORTANT: Jab login page open ho, redirect false karo
+    useEffect(() => {
+        setRedirect(false);
+    }, [setRedirect]);
+
     return (
         <div className={styles.container}>
             <JoinServer />
