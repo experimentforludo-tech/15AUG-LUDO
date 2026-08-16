c46e3813f0b22fe0cb0b298eb7e5b126fcc0bdec
experimentforludo-tech <experimentforludo@gmail.com>
2026-08-16 07:26:00 +0530

    Update constants.js

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -6,4 +6,8 @@ const MOVE_TIME = 15000;
 // classic Ludo layout ke mutabik. In squares pe koi bhi pawn capture nahi ho sakta, akela ho tab bhi.
 const SAFE_POSITIONS = [16, 24, 29, 37, 42, 50, 55, 63];
 
-module.exports = { COLORS, MOVE_TIME, SAFE_POSITIONS };
+// ===== NEW: agar "Play with Friends" wala room 30 minute tak start nahi hota
+// (seat khaali reh gayi, ya sirf host akela baitha reh gaya), to wo apne aap delete ho jayega.
+const LOBBY_CLEANUP_TIME = 30 * 60 * 1000; // 30 minutes
+
+module.exports = { COLORS, MOVE_TIME, SAFE_POSITIONS, LOBBY_CLEANUP_TIME };
